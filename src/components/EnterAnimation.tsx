b30b968e96aaf3c6bc50e6b9b3b21877a85bbb0b
Kleuber Silva <kleuber20@hotmail.com>
2025-03-27 17:46:54 -0300

    substitui o conteúdo da página inicial e adiciona animações de entrada

---
+++ src/components/EnterAnimation.tsx
@@ -0,0 +1,22 @@
+import * as motion from "motion/react-client"
+
+export default function EnterAnimation() {
+    return (
+        <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 1,
+                scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+            }}
+            style={ball}
+        />
+    )
+}
+
+const ball = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#dd00ee",
+    borderRadius: "50%",
+}
